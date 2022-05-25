06e9b30ef787d2a9886f129772d897a531b7ee17
jyothi <jyothibr42@gmail.com>
2022-05-25 09:34:00 +0530

    server

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,12 +1,79 @@
-// import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { IContact } from '../models/IContact';
+import { IGroup } from '../models/IGroup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  // private serverUrl: string = ``;
+  private serverUrl: string = `http://localhost:9000`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+
+  public getAllContacts():Observable<IContact[]> {
+
+    let dataUrl: string = `${this.serverUrl}/contacts`;
+    return this.httpClient.get<IContact[]>(dataUrl).pipe(catchError(this.handleError));
+
+  }
+
+  public getContact(contactId: string): Observable<IContact> {
+    let dataUrl: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.get<IContact>(dataUrl).pipe(catchError(this.handleError));
+  }
+
+  public creatContact(contact: IContact): Observable<IContact> {
+    let dataUrl: string = `${this.serverUrl}/contacts`;
+    return this.httpClient.post<IContact>(dataUrl, contact).pipe(catchError(this.handleError));
+
+  }
+
+  public updateContact(contact: IContact, contactId: string): Observable<IContact> {
+    let dataUrl: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.put<IContact>(dataUrl, contact).pipe(catchError(this.handleError));
+
+  }
+
+  public deleteContact(contactId: string): Observable<{}> {
+    let dataUrl: string = `${this.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.delete<{ }>(dataUrl).pipe(catchError(this.handleError));
+
+  }
+
+  public getAllGroups(): Observable<IGroup[]> {
+    let dataUrl: string = `${this.serverUrl}/groups`;
+    return this.httpClient.get<IGroup[]>(dataUrl).pipe(catchError(this.handleError));
+
+  }
+
+  public getGroup(contact: IContact): Observable<IGroup> {
+    let dataUrl: string = `${this.serverUrl}/groups/${contact.groupId}`;
+    return this.httpClient.get<IGroup>(dataUrl).pipe(catchError(this.handleError));
+  }
+
+
+
+  public handleError(error:HttpErrorResponse) {
+
+    let errorMessage: string = ``;
+    if(error.error instanceof ErrorEvent) {
+      //client error
+      errorMessage = `Error : ${error.error.message}`
+    }
+    else
+    {
+      //server error
+      errorMessage = `Status : ${error.status} \n Message : ${error.message}`
+    }
+    return throwError(errorMessage);
+  }
+
+
+
+
+
 }
